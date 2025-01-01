a68fe702c366cb0acd3089affe3dae29601ee53a
DuncanByrneUT <duncanbyrne7254@gmail.com>
2024-12-31 20:39:44 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# deposit amount function
+def deposit():
+    while True:
+        amount = input("Please enter the amount you'd like to deposit: $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0.")
+        else:
+            print("Please enter a number.")
+    return amount
+
+# main function
+def main():
+    balance = deposit()
+main()
