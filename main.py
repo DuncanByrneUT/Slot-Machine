a02078f0a0bcab598c250975ac9686a2b400ebdc
DuncanByrneUT <duncanbyrne7254@gmail.com>
2024-12-31 20:50:01 -0500

    added the functions to get the number of lines the user wants to bet on and the amount of money that they would like to deposit. terminal prints the amount of $ and lines.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#GLOBAL CONSTANT
+MAX_LINES = 5
+
 # deposit amount function
 def deposit():
     while True:
@@ -12,7 +15,25 @@ def deposit():
             print("Please enter a number.")
     return amount
 
+# number of lines
+def get_number_of_lines():
+    while True:
+        lines = input("Please enter the number of lines you'd like to bet on. (1-" + str(MAX_LINES) + "): ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a number.")
+    return lines
+
+
+
 # main function
 def main():
     balance = deposit()
+    lines = get_number_of_lines()
+    print(balance, lines)
 main()
